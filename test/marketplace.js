d7605199b349c777516517528b1ad1d21d26f060
Marcelo Kopmann <marcelokopmann@gmail.com>
2022-01-18 17:07:49 -0300

    feat: add more marketplace tests

--- test/marketplace.js
+++ test/marketplace.js
@@ -1,5 +1,5 @@
 const { expect } = require("chai");
-const { ethers, BigNumber } = require("hardhat");
+const { ethers } = require("hardhat");
 
 describe("Marketplace", function () {
   let nftContract;
@@ -23,9 +23,9 @@ describe("Marketplace", function () {
 
   beforeEach(deployContractsAndSetAddresses);
 
-  async function mintTokenAndCreateMarketItem(tokenId, price, transactionOptions) {
-    await nftContract.mintToken('');
-    return marketplaceContract.createMarketItem(nftContractAddress, tokenId, price, transactionOptions)
+  async function mintTokenAndCreateMarketItem(tokenId, price, transactionOptions, account = owner) {
+    await nftContract.connect(account).mintToken('');
+    return marketplaceContract.connect(account).createMarketItem(nftContractAddress, tokenId, price, transactionOptions)
   }
 
   it("creates a Market Item", async function () {
@@ -108,4 +108,53 @@ describe("Marketplace", function () {
       .to.be.revertedWith("Please submit the asking price in order to continue");
 
   });
+
+  it("fetches unsold NFT tokens", async function () {
+    // Arrange
+    const price = ethers.utils.parseEther("10");
+    const listingFee = await marketplaceContract.getListingFee();
+    const transactionOptions = { value: listingFee }
+    await mintTokenAndCreateMarketItem(1, price, transactionOptions);
+    await mintTokenAndCreateMarketItem(2, price, transactionOptions);
+
+    // Act
+    const unsoldMarketItems = await marketplaceContract.fetchUnsoldMarketItems();
+
+    // Assert
+    expect(unsoldMarketItems.length).to.equal(2)
+  });
+
+  it("fetches NFT tokens owned by msg.sender ", async function () {
+    // Arrange
+    const price = ethers.utils.parseEther("10");
+    const listingFee = await marketplaceContract.getListingFee();
+    const transactionOptions = { value: listingFee }
+    await mintTokenAndCreateMarketItem(1, price, transactionOptions);
+    await mintTokenAndCreateMarketItem(2, price, transactionOptions);
+    await marketplaceContract.connect(buyer).createMarketSale(nftContractAddress, 1, { value: price });
+
+    // Act
+    const buyerNFTTokens = await marketplaceContract.connect(buyer).fetchMyNFTs();
+
+    // Assert
+    expect(buyerNFTTokens.length).to.equal(1)
+    expect(buyerNFTTokens[0].tokenId).to.equal(1)
+  });
+
+  it("fetches NFT tokens that are listed by msg.sender", async function () {
+    // Arrange
+    const price = ethers.utils.parseEther("10");
+    const listingFee = await marketplaceContract.getListingFee();
+    const transactionOptions = { value: listingFee }
+    await mintTokenAndCreateMarketItem(1, price, transactionOptions);
+    await mintTokenAndCreateMarketItem(2, price, transactionOptions);
+    await mintTokenAndCreateMarketItem(3, price, transactionOptions, buyer);
+
+    // Act
+    const buyerNFTTokens = await marketplaceContract.fetchCreatedItems();
+
+    // Assert
+    expect(buyerNFTTokens.length).to.equal(2)
+  });
+
 });
