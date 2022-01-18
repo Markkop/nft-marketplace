f09b14ae01897d430ca3cf8079815b4f797b1219
Marcelo Kopmann <marcelokopmann@gmail.com>
2022-01-18 16:49:09 -0300

    feat: emit token minting event

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,33 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
-import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
-import '@openzeppelin/contracts/utils/Counters.sol';
-
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 // TO DO: Explain the reason/advantadge to use ERC721URIStorage instead of ERC721 itself
 contract NFT is ERC721URIStorage {
-  using Counters for Counters.Counter;
-  Counters.Counter private _tokenIds;
-  // Counters allow us to keep track of tokenIds
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    // Counters allow us to keep track of tokenIds
 
-  // Address of Marketplace for NFTs to interact
-  address contractAddress;
+    // Address of Marketplace for NFTs to interact
+    address marketplaceAddress;
 
-  constructor(address marketplaceAddress) ERC721('MarkKop', 'MARK') {
-    contractAddress = marketplaceAddress;
+    event TokenMinted(
+        uint256 indexed tokenId,
+        string tokenURI,
+        address marketplaceAddress
+    );
 
-  }
+    constructor(address _marketplaceAddress) ERC721("MarkKop", "MARK") {
+        marketplaceAddress = _marketplaceAddress;
+    }
 
-  function mintToken(string memory tokenURI) public returns (uint) {
-    _tokenIds.increment();
-    uint256 newItemId = _tokenIds.current();
-    _mint(msg.sender, newItemId);
-    _setTokenURI(newItemId, tokenURI);
+    function mintToken(string memory tokenURI) public returns (uint256) {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
 
-    // Give the marketplace approval to transact NFTs between users
-    setApprovalForAll(contractAddress, true);
-    return newItemId;
-  }
+        // Give the marketplace approval to transact NFTs between users
+        setApprovalForAll(marketplaceAddress, true);
+
+        emit TokenMinted(newItemId, tokenURI, marketplaceAddress);
+        return newItemId;
+    }
 }
