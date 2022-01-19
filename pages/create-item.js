6b23600769cd0345915953d7a545656cce953f4d
Marcelo Kopmann <marcelokopmann@gmail.com>
2022-01-19 14:36:19 -0300

    refac: split functions on create item page

--- pages/create-item.js
+++ pages/create-item.js
@@ -3,6 +3,7 @@ import { ethers } from 'ethers'
 import { create as ipfsHttpClient } from 'ipfs-http-client'
 import { useRouter } from 'next/router'
 import Web3Modal from 'web3modal'
+import { useForm } from 'react-hook-form'
 
 import {
   nftaddress, nftmarketaddress
@@ -11,46 +12,35 @@ import {
 import NFT from '../artifacts/contracts/NFT.sol/NFT.json'
 import Market from '../artifacts/contracts/Marketplace.sol/Marketplace.json'
 
-const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0')
+async function uploadDataToIPFS (data) {
+  try {
+    const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0')
+    const added = await client.add(data)
+    return `https://ipfs.infura.io/ipfs/${added.path}`
+  } catch (error) {
+    console.log('Error uploading file: ', error)
+  }
+}
 
 export default function CreateItem () {
   const [fileUrl, setFileUrl] = useState(null)
-  const [formInput, updateFormInput] = useState({ price: '', name: '', description: '' })
+  const { register, handleSubmit } = useForm()
   const router = useRouter()
 
-  async function onChange (e) {
-    const file = e.target.files[0]
-    try {
-      const added = await client.add(
-        file,
-        {
-          progress: (prog) => console.log(`received: ${prog}`)
-        }
-      )
-      const url = `https://ipfs.infura.io/ipfs/${added.path}`
-      setFileUrl(url)
-    } catch (error) {
-      console.log('Error uploading file: ', error)
-    }
-  }
-  async function createMarket () {
-    const { name, description, price } = formInput
-    if (!name || !description || !price || !fileUrl) return
-    /* first, upload to IPFS */
-    const data = JSON.stringify({
-      name, description, image: fileUrl
-    })
-    try {
-      const added = await client.add(data)
-      const url = `https://ipfs.infura.io/ipfs/${added.path}`
-      /* after file is uploaded to IPFS, pass the URL to save it on Polygon */
-      createSale(url)
-    } catch (error) {
-      console.log('Error uploading file: ', error)
-    }
+  async function onFileChange (event) {
+    const file = event.target.files[0]
+    const url = await uploadDataToIPFS(file)
+    setFileUrl(url)
   }
 
-  async function createSale (url) {
+  async function createMarket (name, description, price, assetFileUrl) {
+    if (!name || !description || !price || !assetFileUrl) return
+    const data = JSON.stringify({ name, description, image: assetFileUrl })
+    const metadataUrl = await uploadDataToIPFS(data)
+    await createSale(metadataUrl, price)
+  }
+
+  async function createSale (metadataUrl, price) {
     const web3Modal = new Web3Modal()
     const connection = await web3Modal.connect()
     const provider = new ethers.providers.Web3Provider(connection)
@@ -58,56 +48,67 @@ export default function CreateItem () {
 
     /* next, create the item */
     let contract = new ethers.Contract(nftaddress, NFT.abi, signer)
-    let transaction = await contract.mintToken(url)
+    let transaction = await contract.mintToken(metadataUrl)
     const tx = await transaction.wait()
     const event = tx.events[0]
     const value = event.args[2]
     const tokenId = value.toNumber()
 
-    const price = ethers.utils.parseUnits(formInput.price, 'ether')
+    const priceInWei = ethers.utils.parseUnits(price, 'ether')
 
     /* then list the item for sale on the marketplace */
     contract = new ethers.Contract(nftmarketaddress, Market.abi, signer)
     let listingFee = await contract.getListingFee()
     listingFee = listingFee.toString()
 
-    transaction = await contract.createMarketItem(nftaddress, tokenId, price, { value: listingFee })
+    transaction = await contract.createMarketItem(nftaddress, tokenId, priceInWei, { value: listingFee })
     await transaction.wait()
     router.push('/')
+  }
+
+  async function onSubmit (formData) {
+    const { name, description, price } = formData
+    await createMarket(name, description, price, fileUrl)
   }
 
   return (
     <div className="flex justify-center">
       <div className="w-1/2 flex flex-col pb-12">
-        <input
-          placeholder="Asset Name"
-          className="mt-8 border rounded p-4"
-          onChange={e => updateFormInput({ ...formInput, name: e.target.value })}
-        />
-        <textarea
-          placeholder="Asset Description"
-          className="mt-2 border rounded p-4"
-          onChange={e => updateFormInput({ ...formInput, description: e.target.value })}
-        />
-        <input
-          placeholder="Asset Price in Eth"
-          className="mt-2 border rounded p-4"
-          onChange={e => updateFormInput({ ...formInput, price: e.target.value })}
-        />
-        <input
-          type="file"
-          name="Asset"
-          className="my-4"
-          onChange={onChange}
-        />
-        {
-          fileUrl && (
-            <img className="rounded mt-4" width="350" src={fileUrl} />
-          )
-        }
-        <button onClick={createMarket} className="font-bold mt-4 bg-pink-500 text-white rounded p-4 shadow-lg">
-          Create Digital Asset
-        </button>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            placeholder="Asset Name"
+            className="mt-8 border rounded p-4"
+            name="name"
+            {...register('name')}
+          />
+          <textarea
+            placeholder="Asset Description"
+            className="mt-2 border rounded p-4"
+            name="description"
+            {...register('description')}
+          />
+          <input
+            placeholder="Asset Price"
+            className="mt-2 border rounded p-4"
+            name="price"
+            {...register('price')}
+          />
+          <input
+            type="file"
+            name="Asset"
+            className="my-4"
+            onChange={onFileChange}
+          />
+          {
+            fileUrl && (
+              <img className="rounded mt-4" width="350" src={fileUrl} />
+            )
+          }
+          <button type="submit" className="font-bold mt-4 bg-pink-500 text-white rounded p-4 shadow-lg">
+            Create Digital Asset
+          </button>
+        </form>
+
       </div>
     </div>
   )
