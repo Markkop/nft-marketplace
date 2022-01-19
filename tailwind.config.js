eeea1e1a125046113a4e3229a17591110dab6f5b
Marcelo Kopmann <marcelokopmann@gmail.com>
2022-01-19 13:47:58 -0300

    feat: create initial frontend files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-  content: [],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
+  ],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }
