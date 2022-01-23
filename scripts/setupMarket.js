8b4567a2cb502c9899350fdae0a903285ef64ed7
Marcelo Kopmann <marcelokopmann@gmail.com>
2022-01-23 11:17:32 -0300

    refac: get minted token ids instead of hardcoded

--- scripts/setupMarket.js
+++ scripts/setupMarket.js
@@ -6,6 +6,13 @@ const techEventMetadataUrl = 'https://ipfs.io/ipfs/QmchRqWmRiHP2uXBGxT7sJJUJChDd
 const yellowCrownMetadataUrl = 'https://ipfs.io/ipfs/QmVXBCJcDtgtZfx77W86iG5hrJnFjWz1HV7naHAJMArqNT?filename=undefined'
 const ashleyMetadataUrl = 'https://ipfs.io/ipfs/QmdiA6eywkjMAVGTYRXerSQozLEBA3QpKmAt1E1mKVovhz?filename=undefined'
 
+async function getMintedTokenId (transaction) {
+  const transactionResult = await transaction.wait()
+  const event = transactionResult.events[0]
+  const value = event.args[2]
+  return value.toNumber()
+}
+
 async function setupMarket (marketplaceAddress = MARKETPLACE_CONTRACT_ADDRESS, nftAddress = NFT_CONTRACT_ADDRESS) {
   const marketplaceContract = await hre.ethers.getContractAt('Marketplace', marketplaceAddress)
   const nftContract = await hre.ethers.getContractAt('NFT', nftAddress)
@@ -15,26 +22,30 @@ async function setupMarket (marketplaceAddress = MARKETPLACE_CONTRACT_ADDRESS, n
   const price = hre.ethers.utils.parseEther('10')
   const listingFee = await marketplaceContract.getListingFee()
 
-  await nftContract.mintToken(dogsMetadataUrl)
-  await nftContract.mintToken(techEventMetadataUrl)
-  await marketplaceContract.createMarketItem(nftContractAddress, 1, price, { value: listingFee })
-  await marketplaceContract.createMarketItem(nftContractAddress, 2, price, { value: listingFee })
-  console.log(`${acc1.address} minted tokens 1 and 2 and listed them as market items`)
+  const tx1 = await nftContract.mintToken(dogsMetadataUrl)
+  const tokenId1 = await getMintedTokenId(tx1)
+  const tx2 = await nftContract.mintToken(techEventMetadataUrl)
+  const tokenId2 = await getMintedTokenId(tx2)
+  await marketplaceContract.createMarketItem(nftContractAddress, tokenId1, price, { value: listingFee })
+  await marketplaceContract.createMarketItem(nftContractAddress, tokenId2, price, { value: listingFee })
+  console.log(`${acc1.address} minted tokens ${tokenId1} and ${tokenId2} and listed them as market items`)
 
-  await nftContract.connect(acc2).mintToken(yellowCrownMetadataUrl)
-  await nftContract.connect(acc2).mintToken(ashleyMetadataUrl)
-  await marketplaceContract.connect(acc2).createMarketItem(nftContractAddress, 3, price, { value: listingFee })
-  await marketplaceContract.connect(acc2).createMarketItem(nftContractAddress, 4, price, { value: listingFee })
-  console.log(`${acc2.address} minted tokens 3 and 4 and listed them as market items`)
+  const tx3 = await nftContract.connect(acc2).mintToken(yellowCrownMetadataUrl)
+  const tokenId3 = await getMintedTokenId(tx3)
+  const tx4 = await nftContract.connect(acc2).mintToken(ashleyMetadataUrl)
+  const tokenId4 = await getMintedTokenId(tx4)
+  await marketplaceContract.connect(acc2).createMarketItem(nftContractAddress, tokenId3, price, { value: listingFee })
+  await marketplaceContract.connect(acc2).createMarketItem(nftContractAddress, tokenId4, price, { value: listingFee })
+  console.log(`${acc2.address} minted tokens ${tokenId3} and ${tokenId4} and listed them as market items`)
 
-  await marketplaceContract.createMarketSale(nftContractAddress, 3, { value: price })
-  await nftContract.approve(marketplaceContract.address, 3)
-  await marketplaceContract.createMarketItem(nftContractAddress, 3, price, { value: listingFee })
+  await marketplaceContract.createMarketSale(nftContractAddress, tokenId3, { value: price })
+  await nftContract.approve(marketplaceContract.address, tokenId3)
+  await marketplaceContract.createMarketItem(nftContractAddress, tokenId3, price, { value: listingFee })
   console.log(`${acc1.address} bought token 3 and put it for sale`)
 
-  await marketplaceContract.connect(acc2).createMarketSale(nftContractAddress, 1, { value: price })
-  await nftContract.connect(acc2).approve(marketplaceContract.address, 1)
-  await marketplaceContract.connect(acc2).createMarketItem(nftContractAddress, 1, price, { value: listingFee })
+  await marketplaceContract.connect(acc2).createMarketSale(nftContractAddress, tokenId1, { value: price })
+  await nftContract.connect(acc2).approve(marketplaceContract.address, tokenId1)
+  await marketplaceContract.connect(acc2).createMarketItem(nftContractAddress, tokenId1, price, { value: listingFee })
   console.log(`${acc2.address} bought token 1 and put it for sale`)
 }
 
