d817d12090f81710f39602b8900a469962c4ac4d
Marcelo Kopmann <marcelokopmann@gmail.com>
2022-01-29 22:59:13 -0300

    feat: og meta properties

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,9 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <meta property="og:image" content="https://user-images.githubusercontent.com/16388408/151682517-6c0d89eb-80e6-4eeb-a6b0-6d584cdca5a3.png"/>
+          <meta property="og:title" content="Markkop's NFT Marketplace"/>
+          <meta property="og:description" content="A Fullstack DApp built with Solidity, Hardhat and NextJS"/>
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
           <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🖼️</text></svg>"/>
